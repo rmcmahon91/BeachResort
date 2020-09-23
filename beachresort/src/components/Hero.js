a67f6184faf70fe034ff9d999efa5fb4bb14c997
Ryan McMahon <ryan.mcmahon4@gmail.com>
2020-09-23 12:35:28 -0400

    new upload

--- beachresort/src/components/Hero.js
+++ beachresort/src/components/Hero.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-export default function Hero({children,hero}) {
+export default function Hero({children, hero}) {
     return (
-        <header className={hero}>{children}
-        </header>
+        <header className={hero}>{children}</header>
     )
+}
+
+Hero.defaultProps = {
+    hero: "defaultHero"
 }
--- beachresort/src/pages/Rooms.js
+++ beachresort/src/pages/Rooms.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import { Link } from 'react-router-dom'
+import Banner from '../components/Banner'
 
 export default function Rooms() {
-    return <Hero hero="roomsHero"/>
+    return <Hero hero="roomsHero">
+        <Banner title="Our Rooms">
+            <Link to='/' className='btn-primary'>
+                Return Home
+            </Link>
+        </Banner>
+    </Hero>
 }
